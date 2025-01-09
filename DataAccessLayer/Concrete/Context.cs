862fa9a6528c7d48b52c23f3d5f7ecf6b4d30118
Melekdmr <melekdmrr01@gmail.com>
2025-01-09 17:27:38 +0300

    Add Context class for database connection configuration.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+	public class Context:DbContext
+
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) // bağlantı adresini tutacak olan metot
+		{
+			
+			optionsBuilder.UseSqlServer("server=MELEKDMR\\SQLEXPRESS;database=CorePortfolioDB;integrated security=true");
+
+		}
+
+
+	}
+}
