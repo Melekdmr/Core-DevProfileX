09c2246b4a8515b0e09417ad511215bc0beab1a8
Melekdmr <melekdmrr01@gmail.com>
2025-01-27 18:25:07 +0300

    Updated portfolio with a 'Skills' section and made adjustments to enhance overall presentation.

---
+++ Core_Portfolio/ViewComponents/Skills/SkillList.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace Core_Portfolio.ViewComponents.Skills
+{
+	public class SkillList :ViewComponent
+	{
+		SkillManager skillManager = new SkillManager(new EfSkillDal());
+		public IViewComponentResult Invoke()
+		{
+			var values = skillManager.Getlist();
+			return View(values);
+		}
+	}
+}
