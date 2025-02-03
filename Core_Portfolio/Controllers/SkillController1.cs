b459726445e1217af93640a03478e913dbe0097d
Melekdmr <melekdmrr01@gmail.com>
2025-02-03 22:54:37 +0300

    Refactored the admin panel: Split into components using Partial Views.

---
+++ Core_Portfolio/Controllers/SkillController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Portfolio.Controllers
+{
+	public class SkillController1 : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
