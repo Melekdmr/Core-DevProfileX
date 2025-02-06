366a69f868892e56f5033d717748fa65f5cf358c
Melekdmr <melekdmrr01@gmail.com>
2025-02-06 15:22:15 +0300

    chore(admin): Update Dashboard layout and design

---
+++ Core_Portfolio/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Portfolio.Controllers
+{
+	public class DashboardController : Controller
+	{
+		public IActionResult Index()
+		{
+
+			ViewBag.v1 = "Dashboard ";
+			ViewBag.v2 = "İstatistikler";
+			ViewBag.v3 = "İstatistik Sayfası";
+			return View();
+		}
+	}
+}
