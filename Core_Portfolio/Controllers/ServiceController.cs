e4c23ef05f57d6b328ce7a2c92102e53be38aa92
Melekdmr <melekdmrr01@gmail.com>
2025-02-06 15:05:27 +0300

    feat(admin): Implement CRUD operations for Services section

--- Core_Portfolio/Controllers/ServiceController.cs
+++ Core_Portfolio/Controllers/ServiceController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Portfolio.Controllers
@@ -16,5 +17,52 @@ namespace Core_Portfolio.Controllers
 			var values = serviceManager.Getlist();
 			return View(values);
 		}
+		[HttpGet]
+		public IActionResult AddService()
+		{
+			ViewBag.v1 = "Yetenek Ekleme";
+			ViewBag.v2 = "Yetenekler";
+			ViewBag.v3 = "Yetenek Ekleme";
+			return View();
+
+		}
+		[HttpPost]
+		public IActionResult AddService(Service service)
+		{
+			serviceManager.TAdd(service);
+
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult DeleteService(int id)
+		{
+			var values = serviceManager.TGetByID(id);
+			serviceManager.TDelete(values);
+			return RedirectToAction("Index");
+		}
+		[HttpGet]
+		public IActionResult EditService(int id)
+		{
+			ViewBag.v1 = "Düzenleme";
+			ViewBag.v2 = "Yetenekler";
+			ViewBag.v3 = "Yetenek Güncelleme";
+			var values = serviceManager.TGetByID(id);
+
+			return View(values);
+
+		}
+		[HttpPost]
+		public IActionResult EditService(Service service)
+		{
+
+			ViewBag.v1 = "Düzenleme";
+			ViewBag.v2 = "Yetenekler";
+			ViewBag.v3 = "Yetenek Güncelleme";
+
+			serviceManager.TUpdate(service);
+
+			return RedirectToAction("Index");
+		}
 	}
 }
+
