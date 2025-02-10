8a79db6dfc43abac162a9b8ff1c4c7aa78b371ed
Melekdmr <melekdmrr01@gmail.com>
2025-02-10 21:38:16 +0300

    feat: Introduce user profile section in sidebar

---
+++ Core_Portfolio/ViewComponents/Dashboard/StatisticsDashboard2.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Portfolio.ViewComponents.Dashboard
+{
+	public class StatisticsDashboard2:ViewComponent
+	{
+		Context c = new Context();
+		public IViewComponentResult Invoke()
+		{
+			ViewBag.d1 = c.Portfolios.Count();
+			ViewBag.d2 = c.Messages.Count();
+
+			ViewBag.d3 = c.Services.Count();
+
+			
+			return View();
+		}
+	}
+}
