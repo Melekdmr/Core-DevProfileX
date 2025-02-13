d51ef06cfaf28b72a9e7e9c545f0629a24907006
Melekdmr <melekdmrr01@gmail.com>
2025-02-13 15:52:20 +0300

    Implemented To-Do List UI: Added task input, listing, and checkbox functionality.

---
+++ Core_Portfolio/ViewComponents/Dashboard/ToDoListPanel.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Portfolio.ViewComponents.Dashboard
+{
+	public class ToDoListPanel : ViewComponent
+	{
+		ToDoListManager toDoListManager = new ToDoListManager(new EfToDoListDal());
+
+		public IViewComponentResult Invoke()
+		{
+			var values = toDoListManager.Getlist();
+
+			return View(values);
+		}
+
+	}
+	
+	}
+
