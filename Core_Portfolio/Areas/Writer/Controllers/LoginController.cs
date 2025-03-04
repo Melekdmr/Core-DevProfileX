1f4aee211bc0f936a9c3efff34ed01ad30608052
Melekdmr <melekdmrr01@gmail.com>
2025-03-05 00:39:41 +0300

    ASP.NET Core Identity configuration added. User login functionality implemented using SignInManager for authentication.

---
+++ Core_Portfolio/Areas/Writer/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using Core_Portfolio.Areas.Writer.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Portfolio.Areas.Writer.Controllers
+{
+	[Area("Writer")]
+	public class LoginController : Controller
+	{
+		private readonly SignInManager<WriterUser> _signInManager;
+
+		//Generic Constructor
+		public LoginController(SignInManager<WriterUser> signInManager)
+		{
+			_signInManager = signInManager;
+		}
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Index(UserLoginViewModel p)
+		{
+			if (ModelState.IsValid)
+			{
+				var result = await _signInManager.PasswordSignInAsync(p.UserName, p.Password, true, true);
+				if (result.Succeeded)
+				{
+					//RedirectToAction kullanıldığında, ASP.NET Core yalnızca mevcut "area" içinde
+					//controller arar (eğer bir area içindeyseniz).
+					return Redirect("~/DefaultController1/Index");
+
+
+				}
+				else
+				{
+					ModelState.AddModelError("", "Kullanıcı Adı veya Şifre Hatalı");
+				}
+			}return View();
+		}
+	}
+}
