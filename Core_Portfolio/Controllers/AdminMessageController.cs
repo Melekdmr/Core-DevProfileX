6ab1f280be1de8f25a2fc81ba9cde361df18cf26
Melekdmr <melekdmrr01@gmail.com>
2025-03-25 21:03:50 +0300

    Added admin message management with list, details & delete features

---
+++ Core_Portfolio/Controllers/AdminMessageController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Portfolio.Controllers
+{
+	public class AdminMessageController : Controller
+	{
+		WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+		public IActionResult ReceiverMessageList()
+		{
+			string p = "admin@gmail.com";
+			var values = writerMessageManager.GetListReceiverMessage(p);
+			return View(values);
+		}
+		public IActionResult SendMessageList()
+		{
+			string p = "admin@gmail.com";
+			var values = writerMessageManager.GetListSenderMessage(p);
+			return View(values);
+		}
+		public IActionResult AdminMessageDetails(int id)
+		{
+			var values = writerMessageManager.TGetByID(id);
+			return View(values);
+		}
+		public IActionResult AdminMessageDelete(int id)
+		{
+			var values = writerMessageManager.TGetByID(id);
+			writerMessageManager.TDelete(values);
+			return RedirectToAction("SenderMesageList");
+		}
+	}
+}
