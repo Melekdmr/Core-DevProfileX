0b7d0ef7a35fbb1c9628be3c5ba74b1112e88790
Melekdmr <melekdmrr01@gmail.com>
2025-04-03 16:00:35 +0300

    Added AdminMessageSend action for sending messages

--- Core_Portfolio/Controllers/AdminMessageController.cs
+++ Core_Portfolio/Controllers/AdminMessageController.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Portfolio.Controllers
@@ -28,7 +30,27 @@ namespace Core_Portfolio.Controllers
 		{
 			var values = writerMessageManager.TGetByID(id);
 			writerMessageManager.TDelete(values);
-			return RedirectToAction("SenderMesageList");
+			return RedirectToAction("SendMessageList");
+		}
+		[HttpGet]
+		public IActionResult AdminMessageSend()
+		{
+
+			return View();
+
+		}
+		[HttpPost]
+		public IActionResult AdminMessageSend(WriterMessage p)
+		{
+			p.Sender = "admin@gmail.com";
+			p.SenderName = "Admin";
+			
+			p.Date = DateTime.Now.Date;
+			Context c = new Context();
+			var usernamesurname = c.Users.Where(x => x.Email == p.Receiver).Select(y => y.Name + " " + y.Surname).FirstOrDefault();
+			p.ReceiverName = usernamesurname;
+			writerMessageManager.TAdd(p);
+			return RedirectToAction("SendMessageList");
 		}
 	}
 }
