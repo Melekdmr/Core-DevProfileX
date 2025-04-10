d335e3c941849ce89c495548ff8a5c14335f9207
Melekdmr <melekdmrr01@gmail.com>
2025-04-10 20:38:56 +0300

    Check ImageUrl for null or empty and update from Users table if needed, with fallback to default image

---
+++ Core_Portfolio/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core_Portfolio.ViewComponents.Dashboard
+{
+	
+	public class AdminNavbarMessageList:ViewComponent
+	{
+		WriterMessageManager writerMessage = new WriterMessageManager(new EfWriterMessageDal());
+		public IViewComponentResult Invoke()
+		{
+			 Context c = new Context();
+			string p = "admin@gmail.com";
+			var values = writerMessage.GetListReceiverMessage(p).OrderByDescending(x => x.WriterMessageID).Take(3).ToList();
+
+			foreach (var item in values)
+			{
+				// Eğer ImageUrl null ise Users tablosundan alıyoruz
+				if (string.IsNullOrEmpty(item.ImageUrl))
+				{
+					item.ImageUrl = c.Users.Where(x => x.Email == item.Sender)
+									 .Select(x => x.ImageUrl)
+									 .FirstOrDefault() ?? "/corona-free/dist/assets/images/faces/default.jpg";
+				}
+			}
+
+
+
+
+			return View(values);
+
+		}
+	}
+}
