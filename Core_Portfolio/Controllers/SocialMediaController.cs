e4a6242773f34200230ef7bee5fd5b47b27365cd
Melekdmr <melekdmrr01@gmail.com>
2025-04-10 15:27:20 +0300

    Add dropdown list for 'Status' with 'Aktif' and 'Pasif' options in AddSocialMedia view.

---
+++ Core_Portfolio/Controllers/SocialMediaController.cs
@@ -0,0 +1,59 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+
+namespace Core_Portfolio.Controllers
+{
+	public class SocialMediaController : Controller
+	{
+		SocialMediaManager sociaMediaManager = new SocialMediaManager(new EfSocialMediaDal());
+		public IActionResult Index()
+		{
+			var values = sociaMediaManager.Getlist();
+			return View(values);
+		}
+		[HttpGet]
+		public IActionResult AddSocialMedia()
+		{
+			ViewBag.StatusList = new SelectList(new List<SelectListItem>
+		{
+			new SelectListItem { Text = "Aktif", Value = "true" },
+			new SelectListItem { Text = "Pasif", Value = "false" }
+		}, "Value", "Text");
+
+			return View();
+		}
+		[HttpPost]
+		public IActionResult AddSocialMedia(SocialMedia p)
+		{
+			sociaMediaManager.TAdd(p);
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult DeleteSocialMedia(int id)
+		{
+			var values = sociaMediaManager.TGetByID(id);
+			sociaMediaManager.TDelete(values);
+			return RedirectToAction("Index");
+
+		}
+		[HttpGet]
+		public IActionResult EditSocialMedia(int id)
+		{
+			var values = sociaMediaManager.TGetByID(id);
+		
+	
+			return View(values);
+		}
+		[HttpPost]
+		public IActionResult EditSocialMedia(SocialMedia p)
+		{
+			sociaMediaManager.TUpdate(p);
+			return RedirectToAction("Index");
+		}
+
+	}
+}
