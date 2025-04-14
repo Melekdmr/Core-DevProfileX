4732a5337a288acdfca405ff8f5d805d50d5807d
Melekdmr <melekdmrr01@gmail.com>
2025-04-14 18:41:01 +0300

    Add [AllowAnonymous] attribute to DefaultController1

--- Core_Portfolio/Controllers/DefaultController1.cs
+++ Core_Portfolio/Controllers/DefaultController1.cs
@@ -1,10 +1,12 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Portfolio.Controllers
 {
+	[AllowAnonymous]
 	public class DefaultController1 : Controller
 	{
 		public IActionResult Index()  // IActionResult came in interface format
