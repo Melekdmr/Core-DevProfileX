90aad2b95800f6708955abcfa19a4d976bc769f6
Melekdmr <melekdmrr01@gmail.com>
2025-04-14 11:34:28 +0300

    Add global authorization policy and configure cookie settings (login, access denied, expiration)

--- Core_Portfolio/Program.cs
+++ Core_Portfolio/Program.cs
@@ -1,5 +1,8 @@
 ﻿using DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +11,26 @@ builder.Services.AddDbContext<Context>();
 builder.Services.AddIdentity<WriterUser, WriterRole>().AddEntityFrameworkStores<Context>();
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddMvc(config =>
+{
+	var policy = new AuthorizationPolicyBuilder()
+	.RequireAuthenticatedUser()
+	.Build();
+	config.Filters.Add(new AuthorizeFilter(policy));
+});
+//builder.Services.AddMvc();
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+//	.AddCookie(x =>
+//	{
+//		x.LoginPath = "/AdminLogin/Index/";
+//	});
+builder.Services.ConfigureApplicationCookie(options =>
+{
+	options.Cookie.HttpOnly = true;
+	options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+	options.AccessDeniedPath = "/ErrorPage/Index/";
+	options.LoginPath = "/Writer/Login/Index/";
+});
 
 var app = builder.Build();
 
