380f5de190e6bc0b72910d3d90c892dea55f515e
Melekdmr <melekdmrr01@gmail.com>
2025-04-14 18:43:32 +0300

    Docs: Add inline comments to Program.cs for better code readability

--- Core_Portfolio/Program.cs
+++ Core_Portfolio/Program.cs
@@ -11,25 +11,25 @@ builder.Services.AddDbContext<Context>();
 builder.Services.AddIdentity<WriterUser, WriterRole>().AddEntityFrameworkStores<Context>();
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-builder.Services.AddMvc(config =>
+builder.Services.AddMvc(config => //MVC yapılandırmasını başlatır İçindeki config parametresi, MVC ayarlarını yapılandırmanı sağlar (örn. filtre ekleme).
 {
-	var policy = new AuthorizationPolicyBuilder()
-	.RequireAuthenticatedUser()
-	.Build();
-	config.Filters.Add(new AuthorizeFilter(policy));
-});
-//builder.Services.AddMvc();
-//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-//	.AddCookie(x =>
-//	{
-//		x.LoginPath = "/AdminLogin/Index/";
-//	});
-builder.Services.ConfigureApplicationCookie(options =>
+	var policy = new AuthorizationPolicyBuilder() //Yeni bir yetkilendirme politikası oluşturmak için kullanılır.
+	.RequireAuthenticatedUser() //politikanın, sadece kimliği doğrulanmış (authenticated) kullanıcıların erişimine izin vermesini şart koşar.
+	.Build(); //Politika nesnesini inşa eder.
+	config.Filters.Add(new AuthorizeFilter(policy)); //Tanımlanan bu politikayı bir global yetkilendirme filtresi olarak ekler.
+}); //Böylece tüm controller ve action'larda [Authorize] yazmadan otomatik olarak kimlik doğrulama zorunlu hale gelir.
+	//builder.Services.AddMvc();
+	//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+	//	.AddCookie(x =>
+	//	{
+	//		x.LoginPath = "/AdminLogin/Index/";
+	//	});
+builder.Services.ConfigureApplicationCookie(options => //ASP.NET Core uygulamasında çerez (cookie) tabanlı kimlik doğrulama sisteminin davranışlarını yapılandırmak
 {
-	options.Cookie.HttpOnly = true;
-	options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
-	options.AccessDeniedPath = "/ErrorPage/Index/";
-	options.LoginPath = "/Writer/Login/Index/";
+	options.Cookie.HttpOnly = true; //Cookie sadece sunucu tarafından okunabilir (JS erişemez). Güvenlik sağlar.
+	options.ExpireTimeSpan = TimeSpan.FromMinutes(100); //Cookie’nin süresini belirler (örn. 10 dakika sonra oturumu sonlandır).
+	options.AccessDeniedPath = "/ErrorPage/Index/"; //	Kullanıcının yetkisi olmayan bir sayfaya erişmeye çalıştığında yönlendirileceği hata sayfası.
+	options.LoginPath = "/Writer/Login/Index/";//Giriş yapılmadan korumalı sayfaya erişilirse yönlendirilecek login URL'si.
 });
 
 var app = builder.Build();
