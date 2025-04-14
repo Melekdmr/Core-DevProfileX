29c134920784674d11fbe988ddc2f62ac9ac69cb
Melekdmr <melekdmrr01@gmail.com>
2025-04-14 18:41:49 +0300

    Implement AJAX-based WriterUser list retrieval with JSON response

---
+++ Core_Portfolio/Controllers/WriterUserController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Core_Portfolio.Controllers
+{
+	public class WriterUserController : Controller
+	{
+		WriterUserManager userManager = new WriterUserManager(new EfWriterUserDal());
+		public IActionResult Index()
+		{
+
+			return View();
+		}
+		public IActionResult ListUser()
+		{
+			var values = JsonConvert.SerializeObject(userManager.Getlist());
+
+			return Json(values);
+		}
+		
+	}
+
+
+}
